de80fe0726652acf4a35726b9cff92a98829770a
jrg2705 <jrgonzalez.2705@gmail.com>
2025-09-09 11:18:18 -0400

    fix: Fusionar cabezas de migración en conflicto
    Resuelve el error 'Multiple head revisions' creando un nuevo punto de unión para las ramas de migración divergentes (0f718ec3af27 y 202509052245).

---
+++ migrations/versions/bfb6da0a13a5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bfb6da0a13a5
+Revises: 0f718ec3af27, 202509052245
+Create Date: 2025-09-09 11:16:37.307161
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bfb6da0a13a5'
+down_revision = ('0f718ec3af27', '202509052245')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
